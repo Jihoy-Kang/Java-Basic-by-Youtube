60a6a8a2ce66bf3fcdfff3cae9c9da1c5ef2ea26
Jihyo Kang <luxuryjihyo@naver.com>
2022-05-07 14:31:09 +0900

    07 May 2022

--- src/org/opentutorials/javatutorials/algorithm/Algorithm.java
+++ src/org/opentutorials/javatutorials/algorithm/Algorithm.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class Algorithm {
     public static void main(String[] args) {
-        System.out.println(showTime(9, 10, 10));
-    }
-    public static String showTime(int hour, int min, int sec) {
-        // TODO:
-        Scanner sc = Scanner(System.in);
-        return "현재 시각은 ".concat(Integer.toString(hour)).concat("시 ").concat(Integer.toString(min)).concat("분 ").concat(Integer.toString(sec)).concat("초 입니다.");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char a = sc.
+        char b = sc.next();
+
+
+        for(int i = 1; i < str.length(); i++){
+            result = Math.max(str.charAt(i-1), str.charAt(i));
+        }
+        return result-48;
+
+
     }
 }
---
+++ src/org/opentutorials/javatutorials/algorithm/Codestates.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.algorithm;
+
+public class Codestates {
+    public static void main(String[] args){
+        int intVal = 9;
+        char charVal = Character.forDigit(intVal, 10);
+
+        System.out.println(intVal); // 결과값 : 9
+        System.out.println(charVal); // 결과값 : 9
+    }
+}
