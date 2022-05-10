89a0d1c9d20717e9b860f046691a3d574cc1da14
Jihyo Kang <luxuryjihyo@naver.com>
2022-05-10 14:47:27 +0900

    10 May 2022

--- src/org/opentutorials/javatutorials/algorithm/Algorithm.java
+++ src/org/opentutorials/javatutorials/algorithm/Algorithm.java
@@ -1,14 +1,14 @@
 package org.opentutorials.javatutorials.algorithm;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Algorithm {
     public static void main(String[] args) {
-        String str = "loop";
-        char from = 'o';
-        char to = 'e';
-
-
-            str = str.replace(from,to);
-        System.out.println(str);
+        int num = 5;
+        if(num == 0) System.out.println(false) ;
+        for(int i = 1; i < num;i++){
+            if(num%i == 0) System.out.println(false);
+            else System.out.println(true);
+        }
     }
 }
