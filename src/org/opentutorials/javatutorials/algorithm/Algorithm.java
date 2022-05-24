05417f8300b367024c7db958858106d9a8a8474d
Jihyo Kang <luxuryjihyo@naver.com>
2022-05-24 19:26:07 +0900

    24 May 2022

--- src/org/opentutorials/javatutorials/algorithm/Algorithm.java
+++ src/org/opentutorials/javatutorials/algorithm/Algorithm.java
@@ -1,21 +1,44 @@
 package org.opentutorials.javatutorials.algorithm;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 import java.util.Arrays;
 
 public class Algorithm {
     public static void main(String[] args) {
-        Example example = new Example();
-        Example example2 = new Example(5);
-    }
-}
+        Scanner sc = new Scanner(System.in);
 
-class Example  {
-    public Example() {
-        System.out.println("Example의 기본 생성자 호출!");
-    };
+        int num = sc.nextInt();
+        int i =1;
 
-    public Example(int x) {
-        this();
-        System.out.println("Example의 두 번째 생성자 호출!");
+        List<Integer> prime = new ArrayList<>();
+
+        while(i <= num){
+            prime.clear();
+            int even =sc.nextInt();
+            //소수 구하기
+            for(int h=2; h <= even; h+=2) {
+                for (int j = 1; j <= even; j++) {
+                    if (h % j != 0 && !prime.contains(j)) {
+                        prime.add(j);
+                    }
+                }
+            }
+            System.out.println(prime);
+
+            //장벽구하기
+            loop:
+            for(int a = prime.size()/2; a >= 0; a--){
+                for(int b = a; b < prime.size();b++){
+                    if(prime.get(a)+ prime.get(b) > even){
+                        continue;
+                    }else if(prime.get(a)+ prime.get(b) == even){
+                        System.out.println(prime.get(a) +" "+ prime.get(b));
+                        break loop;
+                    }
+                }
+            }
+                i++;
+        }
     }
 }
--- src/org/opentutorials/javatutorials/algorithm/Codestates.java
+++ src/org/opentutorials/javatutorials/algorithm/Codestates.java
@@ -1,12 +1,28 @@
 package org.opentutorials.javatutorials.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
 public class Codestates {
     public static void main(String[] args){
-        String str = "loop";
-        char from = 'o';
-        char to = 'e';
+        Solution sl = new Solution();
+        int[] arr2 = new int[]{-1, -2, 1, 2, 3, 4, 5};
+        System.out.println(sl.take(5, arr2));
+    }
+}
 
-        str = str.replace(from,to);
-        System.out.println(str);
+class Solution {
+    int[] take(int num, int[] arr){
+        // TODO:
+        if(arr.length == 0 || arr.length <= num) return arr;
+        if(num == 0) return new int[0];
+
+        int[] head = Arrays.copyOfRange(arr, 0, 1);
+        int[] tail = take(num-1, Arrays.copyOfRange(arr, 1, arr.length));
+
+        int[] result = new int[arr.length - (head.length + tail.length)];
+        result = Arrays.copyOfRange(arr, 0, num);
+        return result;
     }
 }
