3b9f4e5ac1ca1c037cb46356f2e427516cd99bbe
Jihyo Kang <luxuryjihyo@naver.com>
2022-05-28 15:20:31 +0900

    28 May 2022

--- src/org/opentutorials/javatutorials/algorithm/Codestates.java
+++ src/org/opentutorials/javatutorials/algorithm/Codestates.java
@@ -7,37 +7,39 @@ import java.util.ArrayList;
 public class Codestates {
     public static void main(String[] args){
         int bufferSize = 1;
-        int capacities = 10;
-        int[] documents = new int[]{7, 4, 5, 6,1,5,2,9,8,7,2,1,};
+        int capacities = 100;
+        int[] documents = new int[]{1, 24, 14, 78, 90};
 
-        int sec = 0;
-        int queCapa = 0;
-        int i = 0;
+        int sec = 1;
+        int i = 1;
 
         ArrayList<Integer> pollQue = new ArrayList<Integer>();
-        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(bufferSize);
+        Queue<Integer> queue = new LinkedList<Integer>();
 
         queue.offer(documents[0]);
         pollQue.add(bufferSize+1);
+        int queCapa = documents[0];
         while (!queue.isEmpty()){
             sec += 1;
-            queCapa = 0;
             if(sec == pollQue.get(0)){
+                queCapa = queCapa - queue.peek();
                 queue.poll();
                 pollQue.remove(0);
             }
-            for(int que : queue){
-                queCapa += que;
-            }
-
-            if((queCapa+documents[i]) <= capacities && i < documents.length-1){
-                queue.offer(documents[i]);
-                pollQue.add(sec+bufferSize);
+            if(i < documents.length){
+                if(queue.size() < bufferSize && (queCapa+documents[i]) <= capacities){
+                    queue.offer(documents[i]);
+                    queCapa = queCapa + documents[i];
+                    pollQue.add(sec+bufferSize);
                     i += 1;
+                }
             }
         }
         System.out.println(sec);
     }
 }
+
+
+
 
 
